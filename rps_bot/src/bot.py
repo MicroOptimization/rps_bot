bb0da20695eb26944e2929b730be14d09f5afc55
MicroOptimization <jackiezhen538@gmail.com>
2020-04-23 16:22:25 -0400

    First commit

---
+++ rps_bot/src/bot.py
@@ -0,0 +1,11 @@
+"""
+===========================================
+Author: Codiacs
+Github: github.com/MicroOptimization
+===========================================
+"""
+print("Sometimes, after a traumatic event, the world just looks completely different.")
+print(".")
+print(".")
+print(".")
+print("Hello completely different looking world.")
