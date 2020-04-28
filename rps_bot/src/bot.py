e665c8b949f8dab69225dfac51af0987b576532d
MicroOptimization <jackiezhen538@gmail.com>
2020-04-28 10:21:27 -0400

    worked on bot logic

--- rps_bot/src/bot.py
+++ rps_bot/src/bot.py
@@ -31,7 +31,41 @@ class MyClient(discord.Client):
         text = message.content
         words = text.split(" ")
         
+        
+        
         challenge_sent = len(words) >= 3 and words[0] == "rps" and words[1] == "challenge"
+        
+        print(len(words) >= 3)
+        
+        print(words[0] == "rps")
+        
+        print(words[1] == "challenge")
+        print()
+        #"""
+        if challenge_sent:
+            self.challenged = client.get_user(int(words[2][3:-1]))
+            self.challenger = message.author
+        
+        
+        print(message.author == self.challenger)
+        print(str(message.author == str(self.challenger)))
+        print(challenge_sent)
+        
+        if challenge_sent and message.author == self.challenger:
+            print("Challenger has messaged Me ---------------------------")
+        else:
+            print("Challenger has not messaged me")
+        
+        if challenge_sent and message.author == self.challenged:
+            print("Challenged has messaged Me ---------------------------")
+        else:
+            print("Challenged has not messaged me")
+        
+        print(message.author)
+        print(self.challenger)
+        print()
+        print(str(message.author))
+        print(str(self.challenger))
         
         if challenge_sent:
             
@@ -46,6 +80,9 @@ class MyClient(discord.Client):
             await asyncio.sleep(0.5)
             
             
+               
+            await message.channel.send("'{} just challenged {} to a game of Rock Paper Scissors'".format(self.challenger, self.challenged))
+         
             await self.challenged.send("You've been challenged by: {}".format(self.challenger))
             await self.challenged.send("Send me your choice:")
             await self.challenged.send("Rock, paper, or scissors?")
@@ -53,8 +90,10 @@ class MyClient(discord.Client):
             await message.author.send("You've challenged: {}".format(self.challenged))
             await message.author.send("Send me your choice:")
             await message.author.send("Rock, paper, or scissors?")
+            
+            
+            
         
-
 client = MyClient()
 
 key = key_retriever.get_key() #Insert your key here
